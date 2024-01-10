a5fca669f1ead9688bb06aac7a260d708f5e9559
Johhei1 <johhni97@gmail.com>
2024-01-10 17:51:26 +0100

    Added Files

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.18.1
+
+using Azure.AI.OpenAI;
+using Azure;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace EchoBot1.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        // Azure AI Search setup
+        static string searchEndpoint = ""; // Add your Azure AI Search endpoint here
+        static string searchKey = ""; // Add your Azure AI Search admin key here
+        static string searchIndexName = ""; // Add your Azure AI Search index name here
+
+        public static ChatCompletionsOptions options = new ChatCompletionsOptions()
+        {
+            Messages = { new ChatMessage(ChatRole.System, @"You are an AI assistant that helps people find information.") },
+            Temperature = (float)0.7,
+            MaxTokens = 200,
+            AzureExtensionsOptions = new AzureChatExtensionsOptions()
+            {
+                Extensions =
+                {
+                    new AzureCognitiveSearchChatExtensionConfiguration()
+                    {
+                        SearchEndpoint = new Uri(searchEndpoint),
+                        IndexName = searchIndexName,
+                        SearchKey = new AzureKeyCredential(searchKey!),
+                    }
+                }
+            }
+        };
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            string prompt = turnContext.Activity.Text;
+            var resultResponse = await CompletionAPIHandler(prompt);
+            if (resultResponse != null)
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text(resultResponse), cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("We are having some trouble communicating with our servers." +
+                    " Please try again later"), cancellationToken);
+            }
+        }
+
+        private async Task<string?> CompletionAPIHandler(string prompt)
+        {
+            string azureOpenAIEndpoint = ""; //Add your azure openai endpoint here
+            string azureOpenAIKey = ""; //Add your azure OpenAi key
+            string userMessage = prompt; 
+
+            OpenAIClient client = new OpenAIClient(
+                new Uri(azureOpenAIEndpoint),
+                new AzureKeyCredential(azureOpenAIKey));
+
+            while (true)
+            {
+                options.Messages.Add(new ChatMessage(ChatRole.User, userMessage));
+
+                Console.WriteLine("Response:");
+                Response<StreamingChatCompletions> response =
+                await client.GetChatCompletionsStreamingAsync(
+                    "",
+                    options);
+
+
+                using StreamingChatCompletions streamingChatCompletions = response.Value;
+                string fullresponse = "";
+                await foreach (StreamingChatChoice choice in streamingChatCompletions.GetChoicesStreaming())
+                {
+                    await foreach (ChatMessage message in choice.GetMessageStreaming())
+                    {
+                        fullresponse += message.Content;
+                    }
+                }
+                options.Messages.Add(new ChatMessage(ChatRole.Assistant, fullresponse));
+                if (fullresponse != null)
+                {
+                    return (string)fullresponse;
+                }
+                else return null;
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and how can I assist you?";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,54 @@
+﻿Bot Framework v4 echo bot sample.
+
+## Prerequisites
+
+- [.NET SDK](https://dotnet.microsoft.com/download) version 6.0
+
+  ```bash
+  # determine dotnet version
+  dotnet --version
+  ```
+
+## To try this sample
+
+- In a terminal, navigate to `EchoBot1`
+
+    ```bash
+    # change into project folder
+    cd # EchoBot1
+    ```
+
+- Run the bot from a terminal or from Visual Studio, choose option A or B.
+
+  A) From a terminal
+
+  ```bash
+  # run the bot
+  dotnet run
+  ```
+
+  B) Or from Visual Studio
+
+  - Launch Visual Studio
+  - File -> Open -> Project/Solution
+  - Navigate to `EchoBot1` folder
+  - Select `EchoBot1.csproj` file
+  - Press `F5` to run the project
+
+## Testing the bot using Bot Framework Emulator
+
+[Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
+
+- Install the Bot Framework Emulator version 4.5.0 or greater from [here](https://github.com/Microsoft/BotFramework-Emulator/releases)
+
+### Connect to the bot using Bot Framework Emulator
+
+- Launch Bot Framework Emulator
+- File -> Open Bot
+- Enter a Bot URL of `http://localhost:3978/api/messages`
+
+### Resources
+
+- Storage/Container Resource
+- Azure OpenAi ChaGPT 
+- Cognitive Search
